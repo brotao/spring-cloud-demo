23ef6c6aca88ed1fff9c313152c05deb5c46939a
brotao <brotao@foxmail.com>
2021-08-18 18:48:12 +0800

    将公共类封装到 cloud-api-commons 公共模块中 添加 eureka 服务模块 将 支付模块和 订单模块注册到 eureka服务中

--- cloud-provider-payment8001/src/main/java/com/brotao/sc/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/brotao/sc/controller/PaymentController.java
@@ -16,7 +16,7 @@ public class PaymentController {
 	private PaymentService paymentService;
 
 	@PostMapping(value = "/payment/create")
-	public CommonResult<Payment> create(Payment payment)
+	public CommonResult<Payment> create(@RequestBody Payment payment)
 	{
 		int result = paymentService.create(payment);
 		log.info("*****插入结果："+result);
