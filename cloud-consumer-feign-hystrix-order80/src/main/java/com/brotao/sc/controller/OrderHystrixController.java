c76db8f38a16f5b8ce0c82b925014fb390a3f7dd
brotao <brotao@foxmail.com>
2021-08-20 21:43:50 +0800

    添加 hytrix，支持服务熔断降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/brotao/sc/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/brotao/sc/controller/OrderHystrixController.java
@@ -1,6 +1,8 @@
 package com.brotao.sc.controller;
 
 import com.brotao.sc.services.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,19 +14,28 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderHystrixController {
-	@Resource
-	private PaymentHystrixService paymentHystrixService;
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
 
-	@GetMapping("/consumer/payment/hystrix/ok/{id}")
-	public String paymentInfo_OK(@PathVariable("id") Integer id) {
-		String result = paymentHystrixService.paymentInfo_OK(id);
-		return result;
-	}
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
 
-	@GetMapping("/consumer/payment/hystrix/timeout/{id}")
-	public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
-		String result = paymentHystrixService.paymentInfo_TimeOut(id);
-		return result;
-	}
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+	@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+	})
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+
+    //善后方法
+    public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id) {
+        return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
 
 }
--- cloud-provider-hygtrix-payment8001/src/main/java/com/brotao/sc/service/PaymentService.java
+++ cloud-provider-hygtrix-payment8001/src/main/java/com/brotao/sc/service/PaymentService.java
@@ -1,26 +1,41 @@
 package com.brotao.sc.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class PaymentService {
 
-	public String paymentInfo_OK(Integer id) {
-		return "线程池: "
-				+ Thread.currentThread().getName()
-				+ "paymentInfo_OK, id: "
-				+ id + "\t"
-				+ "O(∩_∩)O哈哈~";
-	}
 
-	public String paymentInfo_TimeOut(Integer id) {
-		try {
-			TimeUnit.MILLISECONDS.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "O(∩_∩)O哈哈~" + "  耗时(秒): 3";
-	}
+    public String paymentInfo_OK(Integer id) {
+        return "线程池: "
+                + Thread.currentThread().getName()
+                + "paymentInfo_OK, id: "
+                + id + "\t"
+                + "O(∩_∩)O哈哈~";
+    }
+
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
+    public String paymentInfo_TimeOut(Integer id) {
+        try {
+//			TimeUnit.MILLISECONDS.sleep(3000);
+            TimeUnit.MILLISECONDS.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "线程池:  " + Thread.currentThread().getName() + " id:  " + id + "\t" + "O(∩_∩)O哈哈~" + "  耗时(秒): 3";
+    }
+
+    //用来善后的方法
+    public String paymentInfo_TimeOutHandler(Integer id) {
+//        return Arrays.asList("1","2","3");
+		return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
+    }
 }
