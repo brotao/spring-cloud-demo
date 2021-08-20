2a51a8f5e9fe839bce2313e28fb8bf6a95ba7ddf
brotao <brotao@foxmail.com>
2021-08-20 16:19:12 +0800

    添加nacos 注册中心和sentinel 熔断器

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/brotao/sc/PaymentMain9001.java
@@ -0,0 +1,14 @@
+package com.brotao.sc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain9001.class, args);
+	}
+}
